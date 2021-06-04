58ee1af0b2a5a7dff981af6aefd1f2044caa8375
Jordan <jordelquino@gmail.com>
2021-06-04 11:21:37 -0500

    first commit

---
+++ cursor.js
@@ -0,0 +1,76 @@
+// code taken from our tutorial
+// https://www.youtube.com/watch?v=uprZMdVl-aQ
+
+const cursorTag = document.createElement("div")
+const balls = [
+  document.createElement("div"),
+  document.createElement("div"),
+//  document.createElement("div"),
+//  document.createElement("div"),
+//  document.createElement("div"),
+//  document.createElement("div"),
+//  document.createElement("div"),
+//  document.createElement("div"),
+//  document.createElement("div"),
+//  document.createElement("div"),
+//  document.createElement("div"),
+//  document.createElement("div"),
+//  document.createElement("div"),
+//  document.createElement("div"),
+//  document.createElement("div"),
+//  document.createElement("div"),
+//  document.createElement("div"),
+//  document.createElement("div"),
+//  document.createElement("div"),
+//  document.createElement("div"),
+]
+
+cursorTag.classList.add("cursors")
+document.body.append(cursorTag)
+
+let aimX = -30
+let aimY = -30
+
+
+
+balls.forEach((ball, index) => {
+  let currentX = -30
+	let currentY = -30
+
+  cursorTag.append(ball)
+
+	let speed = 0.2 - index * 0.005
+  
+  const animate = function () {
+    currentX += (aimX - currentX) * speed
+    currentY += (aimY - currentY) * speed
+
+    ball.style.left = currentX + "px"
+    ball.style.top = currentY + "px"
+
+    requestAnimationFrame(animate)
+  }
+
+  animate()
+})
+
+document.addEventListener("mousemove", function (event) {
+  aimX = event.pageX
+  aimY = event.pageY
+})
+
+
+// to account for page scroll
+let currentScroll = window.pageYOffset
+let aimScroll = window.pageYOffset
+
+window.addEventListener("scroll", function () {
+  // where are we now we've scrolled
+  currentScroll = window.pageYOffset
+  
+  // update aimY with the difference
+  aimY += currentScroll - aimScroll
+  
+  // reset the aim scroll for next scroll event
+  aimScroll = window.pageYOffset
+})
