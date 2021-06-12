26609e635e95cec3525a966f484f5ab633588f86
Jordan <jordelquino@gmail.com>
2021-06-11 20:35:58 -0500

    Changes in barba js

--- custom.js
+++ custom.js
@@ -6,14 +6,14 @@ barba.init({
 			document.body.classList.add('loading');
 			setTimeout(function(){
 				done();
-			},400);
+			}, 1000);
 		},
 		enter: function(data){
 			var done = this.async();
 			done();
 			setTimeout(function(){
 				document.body.classList.remove('loading');
-			},400);
+			}, 1000);
 		}
 	}]
 });
