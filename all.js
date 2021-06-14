3f0be8e6600b6408613d4fac7ddc5c76d12917e3
Jordan <jordelquino@gmail.com>
2021-06-14 14:06:30 -0500

    New general changes all website

--- all.js
+++ all.js
@@ -81,26 +81,3 @@ window.addEventListener("scroll", function () {
   // reset the aim scroll for next scroll event
   aimScroll = window.pageYOffset
 });
-
-$("nav.about-nav a").on("click", function (event) {
-  
-    //event.preventDefault()
-    
-    const href = $(this).attr("href")
-    
-    window.history.pushState(null, null, href)
-    
-    $.ajax({
-      url: href,
-      success: function (data) {
-        $("main").fadeOut(250, function () {
-          const newPage = $(data).filter("main").html()
-          
-          $("main").html(newPage)
-          
-          $("main").fadeIn(250)
-        })
-      }
-    })
-    
-})
