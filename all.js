16862f8d2e8164a915ad845614760d2ee18ef141
Jordan <jordelquino@gmail.com>
2021-06-14 14:19:18 -0500

    Change header color on scroll

--- all.js
+++ all.js
@@ -81,3 +81,31 @@ window.addEventListener("scroll", function () {
   // reset the aim scroll for next scroll event
   aimScroll = window.pageYOffset
 });
+
+const headerTag = document.querySelector("header")
+
+const toggleHeader = function () {
+  const pixels = window.pageYOffset
+  
+  if (pixels > 60) {
+    headerTag.classList.add("scrolled")
+  } else {
+    headerTag.classList.remove("scrolled")
+  }
+}
+
+const fadeBox = function () {
+  const pixels = window.pageYOffset
+  const alpha = Math.min(pixels / 200, 0.25)
+  
+  
+  headerTag.style.boxShadow = `0 0 10px rgba(0, 0, 0, ${alpha})`
+}
+
+fadeBox()
+toggleHeader()
+
+document.addEventListener("scroll", function () {
+  toggleHeader()
+  fadeBox()
+})
