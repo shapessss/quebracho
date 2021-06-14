ea832526a67df1811e8fc816a621f382b3936fea
Jordan <jordelquino@gmail.com>
2021-06-14 13:13:07 -0500

    Change script position in HTML

--- all.js
+++ all.js
@@ -80,4 +80,28 @@ window.addEventListener("scroll", function () {
   
   // reset the aim scroll for next scroll event
   aimScroll = window.pageYOffset
-})
+});
+
+barba.init({
+	transitions: [{
+		name: 'prueba',
+		leave: function(data){
+			var done = this.async();
+			document.body.classList.add('loading');
+			setTimeout(function(){
+				done();
+			}, 600);
+		},
+		enter: function(data){
+			var done = this.async();
+			done();
+			setTimeout(function(){
+				document.body.classList.remove('loading');
+			}, 600);
+            window.scrollTo(0, 0);
+			reinitTitleScene();
+            reinitScripts();
+			reinitModal();
+		}
+	}]
+});
