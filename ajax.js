62028cc57d2c66dff47dc960a2e36ab6cd016a84
Jordan <jordelquino@gmail.com>
2021-06-14 12:35:11 -0500

    Change ajax classes

--- ajax.js
+++ ajax.js
@@ -1,20 +1,22 @@
-$("nav.about-nav a, a").on("click", function (event) {
-    //event.preventDefault()
-
+$("nav.about-nav a").on("click", function (event) {
+  
+    event.preventDefault()
+    
     const href = $(this).attr("href")
-
+    
     window.history.pushState(null, null, href)
-
+    
     $.ajax({
-        url: href,
-        seccess: function (data) {
-            $("main").fadeOut(250, function () {
-                const newPage = $(data).filter("main").html()
-
-                $("main").html(newPage)
-
-                $("main").fadeIn(250)
-            })
-        }
+      url: href,
+      success: function (data) {
+        $("main").fadeOut(250, function () {
+          const newPage = $(data).filter("main").html()
+          
+          $("main").html(newPage)
+          
+          $("main").fadeIn(250)
+        })
+      }
     })
-})
+    
+  })
