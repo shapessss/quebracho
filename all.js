0ee6f9c2556608b8ae6c6d02fadda5ffb9a2f5d3
Jordan <jordelquino@gmail.com>
2021-06-14 18:46:45 -0500

    Multiple changes in home and modal menu

--- all.js
+++ all.js
@@ -1,10 +1,14 @@
-const toggletag = document.querySelector("#modal-activator")
+const toggleTag = document.querySelector("a#modal-activator")
+const toggleCloser = document.querySelector("div#modal-closer")
 const navTag = document.querySelector("div.about-modal")
-const pageTag = document.querySelector("body")
 
-toggletag.addEventListener("click", function () {
+toggleTag.addEventListener("click", function () {
     navTag.classList.toggle("active")
-    pageTag.classList.toggle("no-overflow")
+    event.preventDefault()
+})
+
+toggleCloser.addEventListener("click", function () {
+    navTag.classList.toggle("active")
 });
 
 // code taken from our tutorial
@@ -83,3 +87,43 @@ window.addEventListener("scroll", function () {
   // reset the aim scroll for next scroll event
   aimScroll = window.pageYOffset
 });
+
+// text animation script
+const animatedTags = document.querySelectorAll("figcaption")
+
+// fade out on load
+animatedTags.forEach(tag => {
+    tag.style.opacity = 0
+  })
+  
+  const fadeIn = function () {
+    // look trhough all the animated tags and see
+    // with the getBoundingClientRect if it's in the window
+    let delay = 0.25
+    
+    animatedTags.forEach(tag => {
+      const tagTop = tag.getBoundingClientRect().top
+      const tagBottom = tag.getBoundingClientRect().bottom
+         
+      if (tagTop < window.innerHeight && tagBottom > 0) {
+        tag.style.animation = `fadein 1s ${delay}s both`
+        delay = delay + 0.25
+      } else {
+        tag.style.opacity = 0
+        tag.style.animation = ""
+      }
+    })
+  }
+  
+  // on load, run fadeIn
+  fadeIn()
+  
+  // on scroll, run fadeIn
+  document.addEventListener("scroll", function () {
+    fadeIn()
+  })
+  
+  // on browser resize, run fadeIn
+  window.addEventListener("resize", function () {
+    fadeIn()
+  })
