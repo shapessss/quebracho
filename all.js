6216f40784ec834cc8a7e6b37f3750eb45dac56e
Jordan <jordelquino@gmail.com>
2021-06-15 15:35:42 -0500

    Adding keyframes

--- all.js
+++ all.js
@@ -89,41 +89,30 @@ window.addEventListener("scroll", function () {
 });
 
 // text animation script
-const animatedTags = document.querySelectorAll("figcaption")
+const animatedTags = document.querySelectorAll("h2, button")
 
-// fade out on load
 animatedTags.forEach(tag => {
-    tag.style.opacity = 0
-  })
-  
-  const fadeIn = function () {
-    // look trhough all the animated tags and see
-    // with the getBoundingClientRect if it's in the window
-    let delay = 0.25
+  tag.style.opacity = 0
+})
+
+const fadeIn = function () {
+  animatedTags.forEach(tag => {
+    const tagTop = tag.getBoundingClientRect().top
+    const tagBottom = tag.getBoundingClientRect().bottom
     
-    animatedTags.forEach(tag => {
-      const tagTop = tag.getBoundingClientRect().top
-      const tagBottom = tag.getBoundingClientRect().bottom
-         
-      if (tagTop < window.innerHeight && tagBottom > 0) {
-        tag.style.animation = `fadein 1s ${delay}s both`
-        delay = delay + 0.25
-      } else {
-        tag.style.opacity = 0
-        tag.style.animation = ""
-      }
-    })
-  }
-  
-  // on load, run fadeIn
+    
+    if (tagTop < window.innerHeight) {
+      tag.style.animation = "fadeIn 1s 0.5s both"
+    }
+  })
+}
+
+fadeIn()
+
+document.addEventListener("scroll", function () {
   fadeIn()
-  
-  // on scroll, run fadeIn
-  document.addEventListener("scroll", function () {
-    fadeIn()
-  })
-  
-  // on browser resize, run fadeIn
-  window.addEventListener("resize", function () {
-    fadeIn()
-  })
+})
+
+window.addEventListener("resize", function () {
+  fadeIn()
+})
