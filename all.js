dcead423a0cb4c19d28329ca029fa59f2d807092
Jordan <jordelquino@gmail.com>
2021-06-28 16:06:59 -0500

    Change pageYOffset height on header

--- all.js
+++ all.js
@@ -144,7 +144,7 @@ const headerTag = document.querySelector("header")
 document.addEventListener("scroll", function () {
   const pixels = window.pageYOffset
 
-  if (pixels > 135) {
+  if (pixels > 145) {
     headerTag.classList.add("scrolled")
   } else {
     headerTag.classList.remove("scrolled")
