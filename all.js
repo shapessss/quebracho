da1322059b6cb0abb8e2a687fab0d13c70e73c02
Jordan <jordelquino@gmail.com>
2021-07-02 23:00:16 -0500

    Testing the finals

--- all.js
+++ all.js
@@ -116,3 +116,8 @@ mailTag.addEventListener("click", function (tomato) {
 
 let vh = window.innerHeight * 0.01;
 document.documentElement.style.setProperty('--vh', `${vh}px`);
+
+window.addEventListener("resize", function () {
+  let vh = window.innerHeight * 0.01;
+  document.documentElement.style.setProperty('--vh', `${vh}px`);
+});
