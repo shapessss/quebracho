d6cc23aa683875890a05738bc74a5eb126c51ecf
Jordan <jordelquino@gmail.com>
2021-07-20 18:51:39 -0500

    Adding services in home page

--- all.js
+++ all.js
@@ -24,7 +24,7 @@ contentTag.addEventListener("click", function() {
 //})
 
 // text animation script
-const animatedTags = document.querySelectorAll("h2, button, section.project__description figcaption, h1.animate_this, nav.about-nav")
+const animatedTags = document.querySelectorAll("h2, button, section.project__description figcaption, h1.animate_this, nav.about-nav, .small-two")
 
 animatedTags.forEach(tag => {
   tag.style.opacity = 0
