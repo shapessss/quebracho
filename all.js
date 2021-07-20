ae7b1f3bdcce951175e661218722b356f3b98ff9
Jordan <jordelquino@gmail.com>
2021-07-20 15:11:54 -0500

    More changes in javascript

--- all.js
+++ all.js
@@ -135,4 +135,4 @@ document.documentElement.style.setProperty('--vh', `${vh}px`);
 window.addEventListener("resize", function () {
   let vh = window.innerHeight * 0.01;
   document.documentElement.style.setProperty('--vh', `${vh}px`);
-});
+})
