5f97a7e95ddf8f5a635733d7f828f328fa027133
Jordan <jordelquino@gmail.com>
2021-07-26 18:28:05 -0500

    Fades for contact ticker

--- all.js
+++ all.js
@@ -24,7 +24,7 @@ contentTag.addEventListener("click", function() {
 //})
 
 // text animation script
-const animatedTags = document.querySelectorAll("h2, button, section.project__description figcaption, h1.animate_this, nav.about-nav, .small-two")
+const animatedTags = document.querySelectorAll("h2, button, section.project__description figcaption, h1.animate_this, nav.about-nav, .small-two, .menu__sec")
 
 animatedTags.forEach(tag => {
   tag.style.opacity = 0
